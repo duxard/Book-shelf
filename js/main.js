e714bb126938c2f7b14319e1aacd0e628e9d0af9
duxard <duxard@gmail.com>
2016-11-04 14:06:52 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#shelf').css({
+        'position' : 'absolute',
+        'left' : '50%',
+        'top' : '50%',
+        'margin-left' : function() {return -$(this).outerWidth()/2},
+        'margin-top' : function() {return -$(this).outerHeight()/2}
+    });
+    $("#sortable").sortable({
+        revert: true
+    }); 
+});
