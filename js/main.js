fb912ac5ccc5332e8d522c6ac1c99ac7e669550f
duxard <duxard@gmail.com>
2016-11-08 13:41:10 +0400

    sec

--- js/main.js
+++ js/main.js
@@ -1,12 +1,43 @@
 $(document).ready(function(){
+    var $goods = $(".shelf-item"),
+        $dropdownMenu = $(".dropdown-content");
+//    shelf centering
     $('#shelf').css({
         'position' : 'absolute',
         'left' : '50%',
-        'top' : '50%',
+        'top' : '45%',
         'margin-left' : function() {return -$(this).outerWidth()/2},
         'margin-top' : function() {return -$(this).outerHeight()/2}
     });
+//    sortable
     $("#sortable").sortable({
-        revert: true
-    }); 
+        revert: true,
+        start: function(event, ui){
+            ui.item.children(".dropdown-content").removeClass("open-menu");
+        }
+    });   
+    
+//    drop-down menu
+    $goods.on('click', function(e){
+        var $ddMenu = $(this).children(".dropdown-content");
+        $dropdownMenu.not($(this).find(".dropdown-content")).each(function(){
+            $(this).removeClass("open-menu");
+        });
+        
+        if($ddMenu.hasClass("open-menu")){
+            $ddMenu.removeClass("open-menu");
+        }else{
+            if(!$(this).hasClass('ui-sortable-helper')){
+               $ddMenu.addClass("open-menu");
+            }
+        }
+    });
+    
+//    click on window closes opened menu
+    $(window).on('click', function(e){
+        var $target = $(e.target);
+        if(!$target.hasClass('shelf-item')){
+            $dropdownMenu.removeClass("open-menu");  
+        }
+    });
 });
